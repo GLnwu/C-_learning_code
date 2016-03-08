5ac80acb4382486da7f1bf1cd1db648c6dd7366e
GLnwu <bingtang1021@gmail.com>
2016-03-08 16:07:17 +0800

    the first cpp source file from Chapter 2 of Cpp Primer Plus

---
+++ myfirst.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name: myfirst.cpp
+    > Author: GaoLi
+    > Mail: bingtang1021@gmail.com 
+    > Created Time: 2016年03月08日 星期二 15时58分24秒
+ ************************************************************************/
+#include<iostream>		//a PREPROCESSOR directive 
+
+int main(void)						//function header
+{							//start of function
+	using namespace std;				//make definitions visible
+	cout << "Come up and C++ me some time.";	//message
+	cout <<endl;					//start a new line
+	cout << "You won't regret it!"<<endl;		//more output
+	
+	return 0;					//terminate main()
+}							//end of function body

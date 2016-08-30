a9ebf42de7b62bb9939a5e354eeeed5d548e4905
GLnwu <bingtang1021@gmail.com>
2016-08-30 20:52:03 +0800

    添加第七章的复习题和编程练习源代码
    创建CHAPTER7目录，存放编程练习和复习题的源代码。

---
+++ CHAPTER7/review_program_8.cpp
@@ -0,0 +1,24 @@
+/*************************************************************************
+    > File Name: review_program_8.cpp
+    > Author: GaoLi
+    > Mail: bingtang1021@gmail.com 
+    > Created Time: Sun 28 Aug 2016 05:21:48 PM CST
+ ************************************************************************/
+#include <iostream>
+#include <cstring>
+#define INVALID_STR (-1)  //return  if pointer is NULL or string length is zero.  
+#define INVALID_CHR (-2)  //no char 'c1' has been found in string 'str'
+/*
+ * Repacle 'char c1' with 'char c2' in character string 'str'
+ * input: @1 str * str @2 char c1 @3 char c2
+ * output: Times which c1 is replaced by c2
+ */
+int replace(char * str,const char c1,const char c2){
+	if(NULL != str || strlen(str)){
+		return INVALID_STR;
+	}
+	unsigned int times_c1_repl = 0;
+	for()
+
+
+}

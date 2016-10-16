c1b09862fdd0c7f201a1dd498f3615b51b683285
GLnwu <bingtang1021@gmail.com>
2016-10-16 17:15:17 +0800

    第八章第6题答案

--- CHAPTER8/Chapter8_8_6.cpp
+++ CHAPTER8/Chapter8_8_6.cpp
@@ -7,33 +7,49 @@
 #include <iostream>
 #include <cstdlib>
 #include "common.h"
+#define MAXLEN 4 //字符指针数组的最大长度
 //计算并返回N个数组的最大值
 template <typename T>
 T maxn(T Array[],const unsigned int Count);
+//maxn的显示具体化，处理字符串指针
+template <> char * maxn<char *>(char * Array[],const unsigned int Count);
 
 int main(void)
 {
 	using namespace std;
 	int A[6];
 	double B[4];
+	char * C[MAXLEN];
 	unsigned int len;
-
-	cout << "Input 5 int number: " << endl;
+	
+	cout << "Input 6 int number: " << endl;
 	GET_ARRAY_LEN(A,len);
-	if(EXIT_SUCCESS == fill_array(A,len))
-	{
+	if(EXIT_SUCCESS == fill_array(A,len)){
 		show_array(A,len);
 		cout << "max number is: " << maxn(A,len) << endl;
-
 	}
 	
+	cout << "Input 4 double number: " << endl;
 	GET_ARRAY_LEN(B,len);
-	if(EXIT_SUCCESS == fill_array(B,len))
-	{
+	if(EXIT_SUCCESS == fill_array(B,len)){
 		show_array(B,len);
 		cout << "max number is: " << maxn(B,len) << endl;
+	}
 
-	}		
+	cout << "Input "<< MAXLEN <<" strings: " << endl;
+	//给字符指针数组分配内存	
+	for(int i = 0;i < MAXLEN;i++){
+		C[i] = new char[MAX_ARRAY_LEN]; 
+	}
+	if(EXIT_SUCCESS == fill_array(C,MAXLEN)){
+		show_array(C,MAXLEN);
+		cout << "max length string is: " << "\"" <<maxn(C,MAXLEN) << "\""<< endl;
+	}
+	//释放字符指针数组分配的内存
+	for(int i = 0;i < MAXLEN;i++){
+		delete [] C[i]; 
+		C[i] = NULL;
+	}
 
 	return EXIT_SUCCESS;
 }
@@ -49,3 +65,20 @@ T maxn(T Array[],const unsigned int Count)
 	return max;
 }
 
+template <> char * maxn<char *>(char * Array[],const unsigned int Count)
+{
+	unsigned int max_len;
+	char * max_len_char = Array[0];
+
+	max_len_char = Array[0];
+	max_len = strlen(Array[0]);
+
+	for(int i = 1;i < Count;i++)
+	{	
+		if(strlen(Array[i]) > max_len){
+			max_len_char = Array[i];
+			max_len = strlen(Array[i]);
+		}
+	}
+	return max_len_char;
+}
--- CHAPTER8/common.h
+++ CHAPTER8/common.h
@@ -7,12 +7,15 @@
 //模板函数的声明与定义，一般都放在头文件中，隐式实例化模板函数时会用到，具体为啥还不清楚
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
+#define MAX_ARRAY_LEN 128
 //获取数组长度
 #define GET_ARRAY_LEN(array,len) {len = (sizeof(array)/sizeof(array[0]));}
 //填充数组内容
 template <typename T>
 int fill_array(T Array[],const unsigned int Number);
-
+//填充数据内容，显示具体化模板，处理字符串指针
+template <> int fill_array<char *>(char * Array[],const unsigned int Number);
 //显示数组内容
 template <typename T>
 int show_array(const T Array[],const unsigned int Number);
@@ -39,33 +42,61 @@ int fill_array(T Array[],const unsigned int Number)
 			cout << "Bad input: input process terminated.\n";
 			break;
 		}
-		else if(0 == temp){
-			break;
-		}
 		Array[i] = temp;
 	}
-	if(Number != i)
-	{
+	if(Number != i){
 		return EXIT_FAILURE; 
 	}
 	return EXIT_SUCCESS;
-
 }
 
+//填充数据内容，显示具体化模板，处理字符串指针
+template <> int fill_array<char *>(char * Array[],const unsigned int Number)
+{
+	using namespace std;
+	char *temp = new char[MAX_ARRAY_LEN];
+	size_t count;
+	int i;
+
+	for (i = 0; i < Number;i++){
+		cout << "Enter value #" << (i + 1) << ": ";
+		cin >> temp;
+		if(!cin){
+			cin.clear();
+			while('\n' != cin.get()){
+				continue;
+			}
+			cout << "Bad input: input process terminated.\n";
+			break;
+		}
+		//最多复制MAX_ARRAY_LEN个字符到字符指针中，
+		//大于等于MAX_ARRAY_LEN个字符时，只复制MAX_ARRAY_LEN - 1个,留一个当作结束符
+		count =( strlen(temp) >= MAX_ARRAY_LEN ) ? (MAX_ARRAY_LEN - 1):(strlen(temp) + 1);
+		temp[strlen(temp)+1] = '\0';
+		strncpy(Array[i],temp,count);
+	}
+	delete [] temp;
+	temp = NULL;
+	if(Number != i){
+		return EXIT_FAILURE; 
+	}
+
+	return EXIT_SUCCESS;
+}
 //显示数组内容
 template <typename T>
 int show_array(const T Array[],const unsigned int Number)
 {
 	using namespace std;
-	if(NULL == Array)
-	{
+	if(NULL == Array){
 		return EXIT_FAILURE;
 	}
 	int i;
-	for(i = 0;i < Number;i++)
-	{
-		cout << Array[i] << " ";
+
+	for(i = 0;i < Number;i++){
+		cout << Array[i] << " " << endl;
 	}
 	cout << endl;
+
 	return EXIT_SUCCESS;
 }

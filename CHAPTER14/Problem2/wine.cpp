592305f52f49feb4e78a999552009618389a70b1
GLnwu <bingtang1021@gmail.com>
2017-09-20 22:21:12 +0800

    Wine的私有继承方式的实现

---
+++ CHAPTER14/Problem2/wine.cpp
@@ -0,0 +1,41 @@
+/*************************************************************************
+    > File Name: wine.cpp
+    > Copyright @2017 <gaoli>
+    > Mail: bingtang1021@gmail.com 
+    > Created Time: Wed 20 Sep 2017 09:38:06 PM CST
+ ************************************************************************/
+#include <iostream>
+#include <string>
+#include "wine.h"
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+// public method of Wine
+void Wine::GetBottles() {
+  if (!years_) {
+    return;
+  }
+  cout << "Enter " <<  (const string &)(*this) << " data"
+    <<"for " << years_ << " year(s):" << endl;
+  for (int i = 0; i < years_; i++) {
+    cout << "Enter year: ";
+    cin >> ((PairArray &)(*this)).first()[i];
+    cout << "Enter bottles for that year: ";
+    cin >> ((PairArray &)(*this)).second()[i];
+  }
+}
+
+void Wine::Show() const {
+  cout << "Wine :" << (const string &)(*this) << endl;
+  cout << "Year    ";
+  cout << "Bottles" << endl;
+  for (int i = 0; i < years_; i++) {
+    cout << ((const PairArray &)(*this)).first()[i] << "    ";
+    cout << ((const PairArray &)(*this)).second()[i] << endl;
+  }
+}
+
+int Wine::sum() {
+  return ((const PairArray &)(*this)).second().sum();
+}
---
+++ CHAPTER14/Problem2/wine.h
@@ -0,0 +1,38 @@
+/*************************************************************************
+    > File Name: wine.h
+    > Copyright @2017 <gaoli>
+    > Mail: bingtang1021@gmail.com 
+    > Created Time: Sun 17 Sep 2017 09:00:55 PM CST
+ ************************************************************************/
+// private inheritance to implement class Wine
+#ifndef CHAPTER14_PROBLEM2_WINE_H_
+#define CHAPTER14_PROBLEM2_WINE_H_
+#include <iostream>
+#include <valarray>
+#include <string>
+#include "pair.h"
+typedef std::valarray<int> ArrayInt;
+typedef Pair<ArrayInt, ArrayInt> PairArray;
+class Wine : private std::string, private PairArray {
+ private:
+  int years_;   // years of the wine
+ public:
+  // initialize lable to l, number of years to y
+  // vintage years to yr[], bottles to bot[]
+  Wine(const char* l, int y, const int yr[], const int bot[])
+    : std::string(l), years_(y) {
+    ((PairArray &)(*this)).first() = ArrayInt(yr, y);
+    ((PairArray &)(*this)).second() = ArrayInt(bot, y);
+  }
+  Wine(const char *l, int y)
+    : std::string(l), years_(y) {
+      ((PairArray &)(*this)).first() = ArrayInt(y);
+      ((PairArray &)(*this)).second() = ArrayInt(y);
+  } 
+  Wine() {}
+  void GetBottles();
+  void Show() const;
+  const std::string & Label() const {return (const std::string & )(*this);}
+  int sum();
+};
+#endif  // CHAPTER14_PROBLEM1_WINE_H_
